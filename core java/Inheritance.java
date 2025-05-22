4ce02eb717b86baeb8ca9c78e9af58e6d9e0846a
savisalanjala <saviezhilarasan17@gmail.com>
2025-05-22 15:26:29 +0530

    Create Inheritance.java

---
+++ core java/Inheritance.java
@@ -0,0 +1,22 @@
+class Animal {
+    public void makeSound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    public void makeSound() {
+        System.out.println("Bark");
+    }
+}
+
+public class InheritanceExample {
+    public static void main(String[] args) {
+        Animal genericAnimal = new Animal();
+        Dog dog = new Dog();
+
+        genericAnimal.makeSound();
+        dog.makeSound();
+    }
+}
