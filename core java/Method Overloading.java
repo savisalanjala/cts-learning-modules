6af4bccb7a509ffc3c999be6b63ef0d8ac978939
savisalanjala <saviezhilarasan17@gmail.com>
2025-05-22 15:11:05 +0530

    Create Method Overloading.java

---
+++ core java/Method Overloading.java
@@ -0,0 +1,16 @@
+public class MethodOverloadingExample {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+    public static double add(double a, double b) {
+        return a + b;
+    }
+    public static int add(int a, int b, int c) {
+        return a + b + c;
+    }
+    public static void main(String[] args) {
+        System.out.println("Sum of two integers: " + add(5, 10));
+        System.out.println("Sum of two doubles: " + add(5.5, 10.2));
+        System.out.println("Sum of three integers: " + add(3, 7, 9));
+    }
+}
