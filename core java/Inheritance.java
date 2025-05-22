b74fd1ffe8593e119437a5b1e59bdf3a9000960e
savisalanjala <saviezhilarasan17@gmail.com>
2025-05-22 15:28:40 +0530

    Update Inheritance.java

--- core java/Inheritance.java
+++ core java/Inheritance.java
@@ -1,3 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        Animal genericAnimal = new Animal();
+        Dog dog = new Dog();
+
+        genericAnimal.makeSound();
+        dog.makeSound();
+    }
+}
+
 class Animal {
     public void makeSound() {
         System.out.println("Animal makes a sound");
@@ -8,15 +18,5 @@ class Dog extends Animal {
     @Override
     public void makeSound() {
         System.out.println("Bark");
-    }
-}
-
-public class InheritanceExample {
-    public static void main(String[] args) {
-        Animal genericAnimal = new Animal();
-        Dog dog = new Dog();
-
-        genericAnimal.makeSound();
-        dog.makeSound();
     }
 }
